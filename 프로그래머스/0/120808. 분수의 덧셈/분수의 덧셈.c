70ae9ec02e175bab992735bf69f8c7435c974abf
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-17 20:45:29 +0900

    [level 0] Title: 분수의 덧셈, Time: 15.11 ms, Memory: 4.38 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int numer1, int denom1, int numer2, int denom2) {
+        int* answer = (int*)malloc(2 * sizeof(int));
+        int numer1_1 = numer1*denom2;
+        int numer2_2 = numer2*denom1;
+        
+        int totalnumer = numer1_1+numer2_2;
+        int totaldenom = denom1*denom2;
+        int bigger =(totalnumer>totaldenom)?totalnumer:totaldenom;
+        
+        while(true){
+            bool nodev = true;
+            for(int i=2;i<=bigger;i++){
+                if (totalnumer%i == 0 && totaldenom%i==0){
+                    totalnumer /= i;
+                    totaldenom /= i;
+                    nodev = false;
+                }
+            }
+            
+            if (nodev == true) break;
+        }
+        answer[0] = totalnumer;
+        answer[1] = totaldenom;
+        return answer;
+    
+}
