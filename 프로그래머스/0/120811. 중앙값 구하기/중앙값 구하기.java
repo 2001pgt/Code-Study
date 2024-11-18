5ae1afee4818884c595451f308ee9c87521615d9
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-18 11:56:03 +0900

    Update 중앙값 구하기.java

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -8,3 +8,32 @@ class Solution {
         return answer;
     }
 }
+// 시프트 연산자 사용
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        return array[array.length >> 1];
+    }
+}
+// 정렬을 직접 구현
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        answer = array[array.length / 2];
+
+        return answer;
+    }
+}
