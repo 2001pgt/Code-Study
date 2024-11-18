d272ad1adbee24bb521438e71262617951a729ba
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-18 12:46:05 +0900

    [level 0] Title: 최빈값 구하기, Time: 1.09 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -6,34 +6,41 @@ class Solution {
         // 정렬
         Arrays.sort(array);
         
-        int count = 0; // 출현 횟수
-        int now = 1; // 현재 값의 출현 횟수
+        int max_count = 0; // 출현 횟수
+        int now_count = 1; // 현재 값의 출현 횟수
         int num = -1; // 최빈값
         boolean same = false; // 최빈값이 중복되는가?
+        // 원소가 1개일 때
         if(array.length == 1){
             answer = array[0];
         }
+        // 한 개 이상일 때
         else{
             for(int i=1;i<array.length;i++){
+                // 현재 원소가 이전 원소와 같으면
                 if(array[i]==array[i-1]){
-                    now += 1; 
-                    if (count == now){
-                        same = true;
+                    now_count += 1; // 현재의 원의 빈도+1
+                    // 최대 빈도와 현재원소 빈도가 같으면
+                    if (max_count == now_count){
+                        same = true; // 최빈값 중복 true
                     }
-                    else if(count < now){
-                        count = now; 
-                        num = array[i];
-                        same = false;
+                    // 현재원소빈도가 최대 빈도보다 크면
+                    else if(max_count < now_count){
+                        max_count = now_count; // 최대 빈도를 현재원소의 빈도수로 바꾸고
+                        num = array[i];// 최빈값을 현재 원소로 바꾼다.
+                        same = false; // 최빈값은 중복되지 않으면 false로 바꾼다.
                     }
                 }
+                // 현재원소가 이전 원소와 달라지면
                 else{
-                    now = 1;
+                    now_count = 1; // 현재 빈도를 초기화
                 }
             }
-            answer = num;
+            answer = num; // 최빈값을 정답에 추가
         }
+        // 만약 최빈값이 중복되면
         if(same){
-            answer = -1;
+            answer = -1; // -1
         }
         
         return answer;
