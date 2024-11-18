26618e1908a706e67033b57524bff87605b202a3
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-18 11:52:41 +0900

    [level 0] Title: 중앙값 구하기, Time: 1.11 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,22 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] array) {
-        // 정수형 ArrayList 생성
-        List<Integer> list = new ArrayList<>();
-        // 새로운 ArrayList에 array의 원소 넣기
-        for (int num : array) {
-            list.add(num); // 박싱
-        }
-        // 정렬
-        Collections.sort(list);
-        // 리스트 크기
-        int size = list.size();
+        Arrays.sort(array);
         int answer = 0;
-        // 중앙값은 크기/2 값의 몫으로 인덱싱
-        answer = list.get(size/2); 
+        answer = array[array.length/2];
         return answer;
     }
 }
