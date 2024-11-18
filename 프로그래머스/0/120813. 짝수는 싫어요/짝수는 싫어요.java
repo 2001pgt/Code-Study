e2f6f4848b1acef7dc9d5d9b64db1d10ae5988c1
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-18 13:00:21 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -4,15 +4,18 @@ import java.util.Collections;
 class Solution {
     public int[] solution(int n) {
         int x;
+        // n 이 홀수일때와 짝수일때 경우 나누기
         if(n%2 == 1){
             x = n/2 + 1;
         }
         else{
             x = n/2;
         }
+        // n의 홀짝 여부에 따라 배열 생성
         int[] array = new int[x];
+        
         for(int i=0;i<x;i++){
-            array[i] = 1+i*2;
+            array[i] = 1+i*2; // 배열에 홀수만 추가
         }
         return array;
     }
