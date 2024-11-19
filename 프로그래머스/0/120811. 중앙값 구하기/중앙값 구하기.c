28880baeec9e092de0112dc6a4e0a39e29ae5ffb
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-19 19:31:55 +0900

    Update 중앙값 구하기.c

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.c
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.c
@@ -20,3 +20,29 @@ int solution(int array[], size_t array_len) {
     answer = array[array_len/2]; 
     return answer;
 }
+// 다른 풀이
+// qsort를 이용한 풀이
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+int compare(const void *a, const void *b)    // 오름차순 비교 함수 구현
+{
+    int num1 = *(int *)a;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+    int num2 = *(int *)b;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+
+    if (num1 < num2)    // a가 b보다 작을 때는
+        return -1;      // -1 반환
+
+    if (num1 > num2)    // a가 b보다 클 때는
+        return 1;       // 1 반환
+
+    return 0;    // a와 b가 같을 때는 0 반환
+}
+
+// array_len은 배열 array의 길이입니다.
+int solution(int array[], size_t array_len) {
+    int answer = 0;
+    qsort(array, array_len, sizeof(int), compare);
+    answer = array[array_len / 2];
+    return answer;
+}
