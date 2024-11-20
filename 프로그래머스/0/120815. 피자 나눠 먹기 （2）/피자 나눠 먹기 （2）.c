cfd63af935a2fa3cced32f8de8580c963cfc33de
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-20 11:33:41 +0900

    Update 피자 나눠 먹기 （2）.c

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.c
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.c
@@ -18,3 +18,16 @@ int solution(int n) {
         }
         return answer;
 }
+// 다른 풀이
+// 최소공배수(LCM) 활
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    // 먼저 n을 나눌 수 있는 최대의 소인수로 나누어 축소합니다.
+    if(n % 2 == 0) n /= 2; // n % 2 == 0이면 n /= 2로 나누고,
+    if(n % 3 == 0) n /= 3; // n % 3 == 0이면 n /= 3으로 나눕니다.
+
+    return n;
+}
