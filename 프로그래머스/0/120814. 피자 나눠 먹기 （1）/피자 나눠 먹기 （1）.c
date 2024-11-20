2e70d5dfcf94bc07ef42e64a1fc4ab4e04f8e2d6
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-20 11:26:24 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0; 
+        int pizza = 1; // 피자 수
+        while(true){
+            // 피자를 7조각 낸것이 사람 수보다 많으면 종료
+            if(n<=pizza*7){
+                answer = pizza;
+                break;
+            }
+            // 사람수 보다 적으면 한판 더 추가 후 반복
+            else{
+                pizza += 1;
+            }
+        }
+        return answer;
+}
