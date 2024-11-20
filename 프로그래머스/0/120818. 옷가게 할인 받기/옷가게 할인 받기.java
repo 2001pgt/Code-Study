1e05c5cd862db7d797df4f65421c3b6fc3e6d1a4
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-20 20:21:15 +0900

    Update 옷가게 할인 받기.java

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,3 +1,6 @@
+/// 풀이 방법 마다 메모리와 속도를 비교해 보았는데 재밌는 결과가 나왔다.
+// 가장 느린 건 else if문을 사용한 풀이였다.
+// 이게 4가지 풀이중 제일 느려서 놀랬다. 뭔가 가장 많이 떠올리는 풀이 같은데 가장 느리고 메모리도 많이 사용했다.
 메모리: 76.3 MB, 시간: 0.04 ms
 class Solution {
     public int solution(int price) {
@@ -14,6 +17,25 @@ class Solution {
         return answer;
     }
 }
+// 2번째로 느린 풀이는 프로그래머스에서 가장 많은 좋아요를 받은 풀이다. 코드가 간결하고 읽기도 편하다.
+// 하지만 속도는 그닥 빠른편이 아니었다.
+메모리: 75.9 MB, 시간: 0.05 ms
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+
+        if(price>=500000) return (int)(price*0.8);
+        if(price>=300000) return (int)(price*0.9);
+        if(price>=100000) return (int)(price*0.95);
+
+        return price;
+    }
+}
+// 3번째 풀이는 나의 풀이다.
+// 처음 문제를 보고 뭔가 이렇게 풀어보고 싶다는 생각이 들었다.
+// 결과는 생각보다 괜찮았다.
+// 위의 2개의 풀이보다 속도도 빠르고 메모리도 적게 사용됐다.
+// 약간 다르긴 하지만 위의 풀이에 while문을 감싼 형태로 보이는데 속도와 메모리는 더적게 나온다. 뭔가 이상하다.
 메모리: 75.7 MB, 시간: 0.03 ms
 class Solution {
     public int solution(int price) {
@@ -40,18 +62,9 @@ class Solution {
         return price;
     }
 }
-메모리: 75.9 MB, 시간: 0.05 ms
-class Solution {
-    public int solution(int price) {
-        int answer = 0;
-
-        if(price>=500000) return (int)(price*0.8);
-        if(price>=300000) return (int)(price*0.9);
-        if(price>=100000) return (int)(price*0.95);
-
-        return price;
-    }
-}
+// 가장 빠르고 메모리도 적게 먹는것은 삼항연산자였다. 
+// 위의 풀이들과 메모리에서 차이가 많이 났다.
+// 하지만 코드의 가독성이 별로인것 같다.
 메모리: 74.3 MB, 시간: 0.03 ms
 class Solution {
     public int solution(int price) {
@@ -61,3 +74,4 @@ class Solution {
         return answer;
     }
 }
+// 왜 이렇게 풀이별로 속도 차이가 나는 걸까 if문이나 else if은 동작형태도 비슷해서 거의 같을줄 알았는데
