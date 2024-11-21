a4468dacf52cfa325bd03c435d93a4025f54b0a0
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-21 11:03:58 +0900

    [level 0] Title: 문자열 뒤집기, Time: 11.28 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,8 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
+        String answer = ""; // 빈 문자열
         for(int i=0;i<my_string.length();i++){
-            answer += my_string.charAt((my_string.length()-1)-i);
+            // for문을 사용해 입력받은 문자열의 마지막 문자부터 차례대로 빈문자열에 삽입
+            answer += my_string.charAt((my_string.length()-1)-i); 
         }
         return answer;
     }
