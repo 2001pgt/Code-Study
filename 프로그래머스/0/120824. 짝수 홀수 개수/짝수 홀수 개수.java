f0e0755d3ba03b344e99a3aabea51d28fc1a561a
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-21 11:42:24 +0900

    Update 짝수 홀수 개수.java

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -15,3 +15,16 @@ class Solution {
         return answer;
     }
 }
+// 다른 풀이
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        for(int i = 0; i < num_list.length; i++)
+            // 이렇게 하면 홀수 일떄 answer[1] 이 나와서 홀수 자리가 +1이 되고 짝수일때도 answer[0]이 나와서 짝수 자리가 +1이 된다.
+            answer[num_list[i] % 2]++;
+
+        return answer;
+    }
+}
