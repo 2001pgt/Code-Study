f298b98ca78fb0237eda90b7d9ece8e212511973
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-21 11:32:10 +0900

    Update 직각삼각형 출력하기.java

--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
@@ -15,3 +15,38 @@ public class Solution {
         }
     }
 }
+
+// 다른 풀이
+// repeat()메서드를 사용한 풀이
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.println("*".repeat(i)); // i번 출력한다.
+        }
+    }
+}
+// 2중 for문을 사용한 풀이
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            // j<i를 설정하여 i번 출력되게하였다.
+            for(int j=0; j<=i; j++){ 
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+// 속도는 셋중에서 2번째 repeat()을 사용한 풀이가 가장 빠르것으로 예상된다.
+// 왜냐하면 다른 풀이들은 2중 반복문이지만 repeat()은 반복문을 한번만 사용하여 속도가 훨씬 빠를것으로 예상 된다.
