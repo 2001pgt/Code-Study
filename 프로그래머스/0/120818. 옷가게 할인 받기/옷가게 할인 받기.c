730f4e7123d470805c797768ea204346139c4dbc
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-21 09:15:15 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int price) {
+    while(true){
+            // 50만원 이상일 때
+            if(price>=500000){
+                price = price*80/100;
+                break;
+            }
+            // 30만원 이상일 때
+            if(price>=300000){
+                price = price*90/100;
+                break;
+            }
+            // 10만원 이상일 때
+            if(price>=100000){
+                price = price*95/100;
+                break;
+            }
+            // 반복하지 않고 종료
+            break;
+        }
+
+        return price;
+}
