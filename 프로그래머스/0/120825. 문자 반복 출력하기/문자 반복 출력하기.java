adaa3275b9c9b97772bb2517a81d4b66d13d0d6d
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-21 12:07:01 +0900

    Update 문자 반복 출력하기.java

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -34,3 +34,17 @@ class Solution {
         return sb.toString(); StringBuilder // 객체의 내용을 String으로 변환하여 반환
     }
 }
+// split()을 사용해서 문자열 배열생성하는 방법
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] str = my_string.split(""); // 입력받은 문자열을 한 글자씩 문자열로 쪼개서 배열로 만든다.
+        for(int i=0; i<my_string.length(); i++){
+            answer += str[i].repeat(n); // repeat를 사용하여 반복
+        }
+        return answer;
+    }
+}
+// 문자열 연결 시, 기존 문자열이 불변(immutable)이므로, 새로운 문자열 객체가 매번 생성되서 성능 저하가 예상된다.
+// StringBuilder를 사용하는 것이 좀 더 효율적일듯
+// 하지만 가독성은 이게 더 좋은듯하다.
