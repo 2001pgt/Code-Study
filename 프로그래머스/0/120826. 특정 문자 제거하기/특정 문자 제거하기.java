204e65d1c4c42017754091110d70bce69d3e0159
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-22 23:11:52 +0900

    Update 특정 문자 제거하기.java

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -14,3 +14,25 @@ class Solution {
         return answer;
     }
 }
+// 다른 풀이
+// replace 메서드 사용
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+
+        answer = my_string.replace(letter, ""); // letter의 문자가 나오면 ""으로 대체
+
+        return answer;
+    }
+}
+// for-each 문 사용
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        for (char c : my_string.toCharArray()) {
+            // 문자형 c 에 문자 하나 씩 저장 후 비교 같지 않으면 answer에 c추가
+            if (c != letter.charAt(0)) answer += c;
+        }
+        return answer;
+    }
+}
