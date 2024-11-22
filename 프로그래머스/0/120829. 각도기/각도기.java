075cdb1ea8b582f51b83b4d5888b9b58d3db7084
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-22 23:18:25 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,15 +1,19 @@
 class Solution {
     public int solution(int angle) {
         int answer = 0;
+        // 평각
         if(angle == 180){
             answer = 4;
         }
+        // 둔각
         else if(angle > 90){
             answer = 3;
         }
+        // 직각
         else if(angle == 90){
             answer = 2;
         }
+        // 예각
         else{
             answer = 1;
         }
