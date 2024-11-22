ac41a0abfceada1673b4c4eae54d52399edfb938
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-22 23:20:24 +0900

    Update 각도기.java

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -20,3 +20,23 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+// 삼항 연산자 사용
+// 간격하고 빠르지만 가독성이 별로다
+class Solution {
+    public int solution(int angle) {
+        return angle == 180 ? 4 : angle < 90 ? 1 : angle == 90 ? 2 : angle > 90 ? 3 : 0;
+    }
+}
+
+// 바로 return하는 방법
+// 더 빠른 속도를 기대함
+class Solution {
+    public int solution(int angle) {
+        if (angle > 0 && angle < 90) return 1;
+        else if (angle == 90) return 2;
+        else if (angle > 90 && angle < 180) return 3;
+        else return 4;
+    }
+}
