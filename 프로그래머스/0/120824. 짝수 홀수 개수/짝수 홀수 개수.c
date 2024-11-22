598fbf011eb7ca8f41e5764aa51d3cbdbdaf7248
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-22 20:01:35 +0900

    Update 짝수 홀수 개수.c

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.c
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.c
@@ -18,3 +18,28 @@ int* solution(int num_list[], size_t num_list_len) {
         }
         return answer;
 }
+
+// 다른 풀이 malloc으로 동적할당
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int* solution(int num_list[], size_t num_list_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int) * 2);
+    int odd_count = 0;
+    int even_count = 0;
+
+    for (int i = 0; i < num_list_len; i++) {
+        if (num_list[i] % 2 == 0) even_count += 1;
+        else odd_count += 1;
+    }
+
+    answer[0] = even_count;
+    answer[1] = odd_count;
+
+    return answer;
+}
+// 둘의 차이는 malloc은 동적 할당시 원소가 초기화 되지않고 쓰레기 값이 들어 갈수 있기 때문에 할당 후 0으로 초기화 하는 작업을 해줘야한다
+// calloc은 동적할당 시 전부 0으로 초기화 시켜버리기때문에 따로 초기화를 하지 않아도 된다.
