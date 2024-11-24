be3c2c896883e05dc1b930c0dd26f8cc4dd375a5
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-24 13:20:20 +0900

    Update 진료 순서 정하기.java

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -25,3 +25,24 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+
+        for(int i = 0; i < answer.length; i++){
+            if(answer[i] != 0){
+                continue;
+            }
+            int idx = 1;
+            for(int j = 0; j < answer.length; j++){
+                if(emergency[i] < emergency[j]){
+                    idx++;
+                }
+            }
+            answer[i] = idx;
+        }
+        return answer;
+    }
+}
