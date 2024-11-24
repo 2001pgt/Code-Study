eb83483729f50cc5ae9cd99be4cceafedb0c91f3
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-24 11:31:23 +0900

    [level 0] Title: 외계행성의 나이, Time: 2.77 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,9 +1,11 @@
 class Solution {
     public String solution(int age) {
         String answer = "";
+        // 정수를 문자열로 형변환
         String strAge = String.valueOf(age);
         
         for(int i=0;i<strAge.length();i++){
+            // 스위치문을 활용해 문자로 변경된 인덱스i의 수를 알파벳으로 변환
             switch(strAge.charAt(i)){
                 case '0':
                     answer += "a";
