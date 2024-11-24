81cf4ca32ce63a738b76edefea632fe82676c6e7
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-24 13:20:38 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,32 +1,3 @@
-import java.util.*;
-
-class Solution {
-    public int[] solution(int[] emergency) {
-        int[] answer = new int[emergency.length];
-        int[] sortedEmergency = emergency.clone();
-        // 내림차순 정렬
-        for(int i=0;i<emergency.length;i++){
-            for(int j=1;j<emergency.length;j++){
-                int tmp = sortedEmergency[j];
-                if(sortedEmergency[j]>sortedEmergency[j-1]){
-                    sortedEmergency[j] = sortedEmergency[j-1];
-                    sortedEmergency[j-1] = tmp;
-                }
-            }
-        }
-        // emergency의 현재값과 같은 값을 sortedEmergency에서 찾아 인덱스+1한 값을 answer에 넣어준다.
-        for(int i=0;i<emergency.length;i++){
-            for(int j=0;j<emergency.length;j++){
-                if(emergency[i] == sortedEmergency[j]){
-                    answer[i] = j+1;
-                }
-            }
-        }
-        return answer;
-    }
-}
-
-// 다른 풀이
 class Solution {
     public int[] solution(int[] emergency) {
         int[] answer = new int[emergency.length];
