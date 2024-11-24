50b14e015e22ff9ca68fa02055e7fae2251529a8
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-25 02:08:37 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        while(hp>0){
+            if(hp>=5){
+                hp -= 5;
+                answer+=1;
+            }
+            else if(hp < 5 && hp>=3){
+                hp -= 3;
+                answer+=1;
+                
+            }
+            else if(hp < 3){
+                hp -= 1;
+                answer+=1;
+            }
+            
+        }
+        return answer;
+    }
+}
