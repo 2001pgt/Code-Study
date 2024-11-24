5408e80f649cb72f0b715ba3640943fcc2636b40
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-11-25 02:10:20 +0900

    Update 개미 군단.java

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -20,3 +20,19 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+class Solution {
+    public int solution(int hp) {
+        // 나머지를 몫을 정답에 추가해고 나머지를 계속해서 나누는 방법
+        int answer = hp / 5;
+        hp %= 5;
+
+        answer += hp / 3;
+        hp %= 3;
+
+        answer += hp / 1;
+
+        return answer;
+    }
+}
