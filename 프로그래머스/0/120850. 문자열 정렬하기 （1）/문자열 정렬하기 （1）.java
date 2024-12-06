daea7a03cf83b2869e27e0dae3a2dcf9c5c686ed
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-06 18:09:56 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.84 ms, Memory: 98 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+class Solution {
+    public int[] solution(String my_string) {
+        // 정수 배열 생성
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for(int i=0;i<my_string.length();i++){
+            // 문자열을 한문자씩 숫자인지확인 후 숫자면 정수 배열에 추가
+            switch(my_string.charAt(i)){
+                case '0':
+                    numbers.add(0);
+                    break;
+                case '1':
+                    numbers.add(1);
+                    break;
+                case '2':
+                    numbers.add(2);
+                    break;
+                case '3':
+                    numbers.add(3);
+                    break;
+                case '4':
+                    numbers.add(4);
+                    break;
+                case '5':
+                    numbers.add(5);
+                    break;
+                case '6':
+                    numbers.add(6);
+                    break;
+                case '7':
+                    numbers.add(7);
+                    break;
+                case '8':
+                    numbers.add(8);
+                    break;
+                case '9':
+                    numbers.add(9);
+            }
+        }
+        Collections.sort(numbers); // 오름차순 정렬
+        int[] answer = numbers.stream().mapToInt(Integer::intValue).toArray(); // 일반 정수 배열로 변환
+        return answer;
+    }
+}
