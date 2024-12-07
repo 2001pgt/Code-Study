0f4baaf3d8e6f50298164b76a04104a951be75a7
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-08 01:49:40 +0900

    Update 숨어있는 숫자의 덧셈 （1）.java

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -43,3 +43,16 @@ class Solution {
         return answer;
     }
 }
+// 다른 풀이
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String str = my_string.replaceAll("[^0-9]",""); // replaceAll로 문자 제거
+
+        for(char ch : str.toCharArray()) {
+            answer += Character.getNumericValue(ch);
+        }
+
+        return answer;
+    }
+}
