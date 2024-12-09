bf28bd5dfc369136d816b39775ed771d2c6784fb
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-09 20:50:10 +0900

    Update 컨트롤 제트.java

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,3 +1,41 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String buffer = "";
+        int number = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+
+            if (currentChar == ' ') {
+                // 공백인 경우: buffer를 숫자로 변환하고 answer에 더하기
+                if (!buffer.isEmpty()) {
+                    number = Integer.parseInt(buffer);
+                    answer += number;
+                    buffer = "";
+                }
+            } else if (currentChar == 'Z') {
+                // Z인 경우: 바로 이전에 추가한 숫자를 빼기
+                answer -= number;
+                buffer = ""; // buffer는 비워줌
+            } else {
+                // 숫자인 경우: buffer에 추가
+                buffer += currentChar;
+            }
+        }
+
+        // 마지막 숫자 처리
+        if (!buffer.isEmpty()) {
+            number = Integer.parseInt(buffer);
+            answer += number;
+        }
+
+        return answer;
+    }
+}
+// 다른 풀이
+// 뭔가 스택을 쓰면 더쉽게 풀수있을거 같아서 풀어봤더니 엄청간단하게 풀린다.
+// 속도도 0ms 대가 나오며 10배 가까이 빨라짐
 import java.util.Stack;
 
 class Solution {
