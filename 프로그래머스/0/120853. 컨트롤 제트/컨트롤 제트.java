eed52de4df1ffb4fac7f4823af79f92a303c3480
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-09 20:48:47 +0900

    Update 컨트롤 제트.java

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -33,3 +33,33 @@ class Solution {
         return answer;
     }
 }
+// 다른 풀이
+// 뭔가 스택을 쓰면 더쉽게 풀수있을거 같아서 풀어봤더니 엄청간단하게 풀린다.
+import java.util.Stack;
+
+class Solution {
+    public int solution(String s) {
+        Stack<Integer> stack = new Stack<>();
+        String[] tokens = s.split(" ");
+
+        for (String token : tokens) {
+            if (token.equals("Z")) {
+                // Z인 경우: 스택에서 마지막 숫자를 제거
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                // 숫자인 경우: 스택에 추가
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        // 스택에 남아 있는 모든 숫자를 합산
+        int answer = 0;
+        for (int num : stack) {
+            answer += num;
+        }
+
+        return answer;
+    }
+}
