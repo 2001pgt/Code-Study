602f7ce490964507b029562226afd11d3c3f468b
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-11 21:23:48 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.20 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String my_string) {
+        
+         StringBuilder sb = new StringBuilder();
+        // 문자열을 순회
+        // 빈 문자열 안에 문자가 없으면 넣고 중복되면 스킵
+        for(int i=0;i<my_string.length();i++)
+        {   
+            // indexOf의 문자열이 sb안에 없으면 -1을 반환함을 이용
+            // -1 반환시 문자추가
+            if(sb.indexOf(String.valueOf(my_string.charAt(i)))==-1)
+            {
+                sb.append(my_string.charAt(i));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
