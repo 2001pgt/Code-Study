2554f512f11830393e7c4da5fe1242972a205e98
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-14 23:40:17 +0900

    Update 369게임.java

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -19,3 +19,20 @@ class Solution {
         return answer;
     }
 }
+
+// 다른풀이
+// 자바에서 + 연산자는 두 개의 피연산자 중 하나가 문자열이면, 다른 피연산자를 문자열로 자동 변환한 후 문자열 결합을 수행
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+
+        String str = order+""; // 문자열로 변환
+
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if(c=='3'||c=='6'||c=='9') answer++;
+        }
+
+        return answer;
+    }
+}
