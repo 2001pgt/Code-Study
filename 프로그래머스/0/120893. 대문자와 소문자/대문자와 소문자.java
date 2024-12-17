5a5c463a196395ae5877553c5f7d5a0f509977a4
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-17 12:17:38 +0900

    Update 대문자와 소문자.java

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -13,3 +13,24 @@ class Solution {
         return sb.toString();
     }
 }
+
+// 다른풀이
+// 아스키코드에서 대소문자 차이가 32나는것을 이용
+// 대문자면 +32 소문자면 -32
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>=97 && s.charAt(i)<=122){
+                answer += (char)(s.charAt(i)-32);
+            } else if(s.charAt(i)>=65 && s.charAt(i)<=90){
+                answer += (char)(s.charAt(i)+32);
+            } else {
+                answer += s.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+}
