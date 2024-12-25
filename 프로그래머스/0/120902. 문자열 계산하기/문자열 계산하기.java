31239f8bb33f530e6c127c72f67a397f07d02a5c
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-25 17:46:53 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.15 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,22 +1,24 @@
 class Solution {
     public int solution(String my_string) {
-        // 공백을 기준으로 배열로 나눈다
+        // 공백을 기준으로 문자열 배열로 나눈다
         // 각각의 원소가 연산자일때와 아닐때로 구분한뒤
         // 아닐때는 해당 원소를 정수로 변환하여 부호에 맞게 더한다.
         boolean operator = true;
         int num = 0;
         int answer = 0;
-        String[] numbers = my_string.split(" ");
-        System.out.println(numbers);
+        String[] numbers = my_string.split(" "); // 문자열 배열로 변환
         for(int i=0;i<numbers.length;i++){
+            // 더하기 일때
             if(numbers[i].equals("+")){
                 operator = true;
                 continue;
             }
+            // 빼기 일때
             else if(numbers[i].equals("-")){
                 operator = false;
                 continue;
             }
+            // 정수 일때
             else{
                 num = Integer.valueOf(numbers[i]);
                 if (operator) {
