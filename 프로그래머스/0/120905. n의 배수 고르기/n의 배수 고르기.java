cf4d48fc8ec5046ce40a8bd3daf979c9bad789bd
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-26 17:58:15 +0900

    [level 0] Title: n의 배수 고르기, Time: 5.24 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        // ArrayList 생성 후 
+        // for문으로 배열을 순회하면서 n으로 나눴을 때 0으로 나눠떨어지면 ArrayList에 추가
+        // 정수 배열로 바꿔서 리턴
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<numlist.length;i++){
+            if(numlist[i] % n == 0){
+                list.add(numlist[i]);
+            }
+        }
+        int[] intArray = list.stream().mapToInt(Integer::intValue).toArray();
+        return intArray;
+    }
+}
