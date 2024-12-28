c5eb0448c01c65e7f50da149289385f20e0e59b2
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-28 17:27:16 +0900

    Update 문자열안에 문자열.java

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -10,3 +10,27 @@ class Solution {
             return 2;
     }
 }
+// 다른 풀이
+// 메서드 사용하지 않고 직접 구현하기
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+
+        boolean result = false;
+        for (int i = 0; i < str1.length(); i++) {
+            char chr = str1.charAt(i);
+
+            if (chr == str2.charAt(0)) {
+                int len = i + str2.length();
+                String str = (len < str1.length() ? str1.substring(i, len) : str1.substring(i));
+
+                if (str.equals(str2)) { result = true; break; }
+            }
+        }
+
+        if (result) answer = 1; 
+        else answer = 2;
+
+        return answer;
+    }
+}
