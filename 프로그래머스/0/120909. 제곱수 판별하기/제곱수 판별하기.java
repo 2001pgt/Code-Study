84ea4541a533388ef317a8210136528de451b522
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-28 17:39:09 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int n) {
-        // for문 1~1000까지 돌려서 확인한다.
+        // for문 1~1000까지 돌려서 확인한다.//
         int answer = 2;
         for(int i=1;i<=1000;i++)
         {
