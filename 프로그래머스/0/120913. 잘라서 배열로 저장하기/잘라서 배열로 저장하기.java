bfa1bfe817126ed3a88e3ae363de57f94045228b
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-29 10:23:08 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.06 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -1,14 +1,16 @@
 class Solution {
     public String[] solution(String my_str, int n) {
-        int resultCnt = (my_str.length() + n - 1) / n;
-        String[] answer = new String[resultCnt];
-
-        for (int i = 0; i < resultCnt; i++) {
-            int start = n * i;
-            int end = start + n >= my_str.length()? my_str.length(): start + n;
-            answer[i] = my_str.substring(start, end);
+    StringBuilder sb = new StringBuilder();
+    int size = (my_str.length() + n - 1) / n;
+    String[] strarr = new String[size];
+    int index = 0;
+    for (int i = 0; i < my_str.length(); i++) {
+        sb.append(my_str.charAt(i));
+        if ((i + 1) % n == 0 || i == my_str.length() - 1) {
+            strarr[index++] = sb.toString();
+            sb.setLength(0); // StringBuilder 초기화
         }
-
-        return answer;
     }
+    return strarr;
+}
 }
