f1c109cdcfd9a4ed0d5666bb55dd00dec9105878
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-29 09:50:33 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -1,20 +1,14 @@
 class Solution {
     public int solution(int[] array) {
-        // 배열의 원소를 추출해서 문자열로 변경
-        // 문자열을 다시 문자 배열로 변경 거기서 '7'의 횟수를 카운트 
-        int count = 0;
-        for(int i=0;i<array.length;i++)
-        {
-            String str = String.valueOf(array[i]);
-            char[] charArray = str.toCharArray();
-            for(int j=0;j<str.length();j++)
-            {
-                if(charArray[j] == '7')
-                {
-                    count++;
+        int answer = 0;
+        for(int a : array){
+            while(a != 0){
+                if(a % 10 == 7){
+                    answer++;
                 }
+                a /= 10;
             }
         }
-        return count;
+        return answer;
     }
 }
