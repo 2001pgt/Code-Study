e0109bf736f0d7705b1640bb263a164f2524a93c
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-29 10:14:57 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -1,27 +1,14 @@
 class Solution {
     public String[] solution(String my_str, int n) {
-        // for문으로 문자열변수에 my_str[i] 문자를 추가
-        // i가 n번 반복될 때 마다 문자열을 비우기
-        // 만들어진 문자열을 문자열배열에 추가
-        // 만약 문자가 남으면 그대로 추가후 종료
-        String str = "";
-        int size = my_str.length()%n == 0 ?  my_str.length()/n : my_str.length()/n+1;
-        String[] strarr = new String[size];
-        int index = 0;
-        for(int i=0;i<my_str.length();i++)
-        {
-            str+=my_str.charAt(i);
-            if(i == my_str.length()-1){
-                strarr[index] = str;
-            }
-                
-            else if((i+1) % n == 0)
-            {
-                strarr[index] = str;
-                str = "";
-                index++;
-            }
+        int resultCnt = (my_str.length() + n - 1) / n;
+        String[] answer = new String[resultCnt];
+
+        for (int i = 0; i < resultCnt; i++) {
+            int start = n * i;
+            int end = start + n >= my_str.length()? my_str.length(): start + n;
+            answer[i] = my_str.substring(start, end);
         }
-        return strarr;
+
+        return answer;
     }
 }
