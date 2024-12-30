73e2587d53e032a2e13e8837f29f7ff42a3b3f84
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-30 20:42:00 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.42 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int[] numbers) {
-        int max = -100000000;
+        int max = numbers[0]*numbers[1];
         for(int i=0;i<numbers.length;i++)
         {
             for(int j=0;j<numbers.length;j++)
