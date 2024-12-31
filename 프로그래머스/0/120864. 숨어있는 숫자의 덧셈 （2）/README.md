77a65d9b8511a9b55f9536340798db646ead792d
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-31 18:28:48 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.27 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/README.md
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숨어있는 숫자의 덧셈 (2) - 120864 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864#) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 0.09 ms
+메모리: 80.1 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 31일 18:24:54
+2024년 12월 31일 18:28:44
 
 ### 문제 설명
 
--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,63 +1,13 @@
 class Solution {
     public int solution(String my_string) {
-        // 자연수가 없는경우 0 리턴
-        // 스트링빌더 객체 생성
-        // 숫자면 객체에 append
-        // 객체안 숫자가 공백이 아니라면 객체안에 숫자들을 정수로 변환해서 결과에 더한다
-        // 만약 "" 이면 continue
-        StringBuilder sb = new StringBuilder();
         int answer = 0;
-        if(my_string.equals(""))
-        {
-            return 0;
+
+        String[] str = my_string.replaceAll("[a-zA-Z]", " ").split(" ");
+
+        for(String s : str){
+            if(!s.equals("")) answer += Integer.valueOf(s);
         }
-        for(int i=0;i<my_string.length();i++)
-        {
-            switch(my_string.charAt(i))
-            {
-                case '0':
-                    sb.append(my_string.charAt(i));
-                    break;
-                case '1':
-                    sb.append(my_string.charAt(i));
-                    break;
-                case '2':
-                    sb.append(my_string.charAt(i));
-                    break;
-                case '3':
-                    sb.append(my_string.charAt(i));
-                    break;
-                case '4':
-                    sb.append(my_string.charAt(i));
-                    break;
-                case '5':
-                    sb.append(my_string.charAt(i));
-                    break;
-                case '6':
-                    sb.append(my_string.charAt(i));
-                    break;
-                case '7':
-                    sb.append(my_string.charAt(i));
-                    break;
-                case '8':
-                    sb.append(my_string.charAt(i));
-                    break;
-                case '9':
-                    sb.append(my_string.charAt(i));
-                    break;
-                default:
-                    if(!sb.toString().equals(""))
-                    {
-                        answer += Integer.valueOf(sb.toString());
-                        sb.setLength(0); // sb 초기화
-                    }
-                    break;
-            }
-        }
-        if(!sb.toString().equals(""))
-        {
-            answer += Integer.valueOf(sb.toString());
-        }
+
         return answer;
     }
 }
