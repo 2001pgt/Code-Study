9cc8f2091cf7599356a6ff3bd26ced88841c9770
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-31 18:29:40 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.05 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,11 +1,36 @@
 class Solution {
-    public int solution(String my_string) {
+    public static int solution(String my_string) {
         int answer = 0;
 
-        String[] str = my_string.replaceAll("[a-zA-Z]", " ").split(" ");
+        int idx = 0;
+        while (idx < my_string.length()) {
+            int charNum = my_string.charAt(idx);
 
-        for(String s : str){
-            if(!s.equals("")) answer += Integer.valueOf(s);
+            // 숫자로 시작하는 부분 탐색
+            if (charNum >= (int) '0' && charNum <= (int) '9') {
+                int len = 1;
+
+                // 계속 해서 숫자가 나오는지 확인
+                if (idx + len < my_string.length()) {
+                    while (my_string.charAt(idx + len) >= (int) '0'
+                            && my_string.charAt(idx + len) <= (int) '9') {
+                        len++;
+                        if (idx + len == my_string.length()) {
+                            break;
+                        }
+                    }
+                }
+
+                // 숫자로 변환, 덧셈
+                answer += Integer.parseInt(my_string.substring(idx, idx + len));
+
+                idx += len;
+            }
+
+            if (idx == my_string.length() - 1) {
+                return answer;
+            }
+            idx++;
         }
 
         return answer;
