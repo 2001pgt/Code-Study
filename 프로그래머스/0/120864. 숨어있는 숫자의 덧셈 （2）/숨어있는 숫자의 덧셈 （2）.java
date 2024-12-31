91572ad009ba47053cd8a06d0c4f61109656e172
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-12-31 18:30:38 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,38 +1,21 @@
 class Solution {
-    public static int solution(String my_string) {
+    public int solution(String my_string) {
         int answer = 0;
-
-        int idx = 0;
-        while (idx < my_string.length()) {
-            int charNum = my_string.charAt(idx);
-
-            // 숫자로 시작하는 부분 탐색
-            if (charNum >= (int) '0' && charNum <= (int) '9') {
-                int len = 1;
-
-                // 계속 해서 숫자가 나오는지 확인
-                if (idx + len < my_string.length()) {
-                    while (my_string.charAt(idx + len) >= (int) '0'
-                            && my_string.charAt(idx + len) <= (int) '9') {
-                        len++;
-                        if (idx + len == my_string.length()) {
-                            break;
-                        }
-                    }
+        int ad = -1;
+        for(char c : my_string.toCharArray()){
+            int cInt =  c;
+            if(cInt >= 47 && cInt <= 57){
+                if(ad == -1) ad = cInt -48;
+                else ad = ad * 10 + (cInt-48);
+            }
+            else{
+                if(ad != -1){
+                    answer += ad;
+                    ad = -1;
                 }
-
-                // 숫자로 변환, 덧셈
-                answer += Integer.parseInt(my_string.substring(idx, idx + len));
-
-                idx += len;
             }
-
-            if (idx == my_string.length() - 1) {
-                return answer;
-            }
-            idx++;
         }
-
+        if(ad != -1) answer += ad;
         return answer;
     }
 }
