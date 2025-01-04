4b4e4fedaf0dae4485d20d8ce36c69d53b529be8
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-01-04 19:14:18 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,40 @@
+import java.math.BigInteger;
+
+class Solution {
+    public static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    public int solution(int a, int b) {
+        int gcd = findGCD(a,b);
+        int b_gcd = b / gcd;
+        while(true)
+        {   
+            if(b_gcd % 2 == 0)
+            {
+                b_gcd /= 2;
+            }
+            else if(b_gcd % 5 == 0)
+            {
+                b_gcd /=5;
+            }
+            else
+            {
+                if(b_gcd != 1)
+                {
+                    return 2;
+                }
+                else
+                {
+                    break;
+                }
+            }       
+        }
+        return 1;
+    }
+}
