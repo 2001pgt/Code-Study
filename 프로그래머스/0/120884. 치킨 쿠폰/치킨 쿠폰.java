3eca15fad5cf9353595649844b2f9b3dcfd769a1
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-01-09 11:01:26 +0900

    Update 치킨 쿠폰.java

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,3 +1,20 @@
+메모리: 85.7 MB, 시간: 3.94 ms
+class Solution {
+    public int solution(int chicken) {
+        int service = 0;
+        int coupon = 0;
+        for(int i=0;i<chicken;i++)
+        {
+            coupon += 1;
+            if(coupon % 10 == 0){
+                service += 1;
+                coupon += 1;
+            }
+        }
+        return service;
+    }
+}
+(0.03ms, 74.6MB)
 class Solution {
     public int solution(int chicken) {
         int answer = chicken/9;
