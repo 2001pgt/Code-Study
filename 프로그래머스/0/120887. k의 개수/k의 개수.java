2c663397571d0be7ee152f5d1cfeea599cfc117e
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-01-12 19:31:43 +0900

    [level 0] Title: k의 개수, Time: 3.63 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,14 +1,13 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        // i~j사이의 수를 문자열로 만든다.
-        // 그 수의 문자를 하나씩 k와 비교해 같을때 마다 answer++
         int answer = 0;
-        for(int x=i;x<=j;x++){
-            String str = String.valueOf(x);
-            for(int y=0;y<str.length();y++){
-                if(String.valueOf(str.charAt(y)).equals(String.valueOf(k))){
+
+        for (int num = i; num <= j; num++){
+            int tmp = num;
+            while (tmp != 0){
+                if (tmp % 10 == k)
                     answer++;
-                }
+                tmp /= 10;
             }
         }
         return answer;
