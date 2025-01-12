c89b76c2ab8aca0abf593f643740746a7d9d245a
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-01-12 19:12:07 +0900

    [level 0] Title: A로 B 만들기, Time: 1.08 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,20 +1,11 @@
+import java.util.Arrays;
 class Solution {
     public int solution(String before, String after) {
-        // before 첫 문자부터 마지막 문자까지 after안에 있는지 확인한다.
-        // 만약 있다면 해당 위치의 after문자를 삭제
-        // 없다면 0 반환
-        // 전부 통과 하면 1 반환
-        StringBuilder sb = new StringBuilder(after);
-        for(int i=0;i<before.length();i++){
-            
-            if(sb.indexOf(String.valueOf(before.charAt(i))) == -1){
-                return 0;
-            }
-            else{
-                sb.deleteCharAt(sb.indexOf(String.valueOf(before.charAt(i))));  
-            }
-            
-        }
-        return 1;
+        char[] a = before.toCharArray();
+        char[] b = after.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return new String(a).equals(new String(b)) ? 1 :0;
     }
 }
