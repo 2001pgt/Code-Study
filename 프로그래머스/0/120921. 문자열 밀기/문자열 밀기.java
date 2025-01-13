2d09cd9411546e37b369079f6ad93de2f8a40842
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-01-13 20:27:04 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,21 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        // 맨앞에 마지막 문자를 삽입 마지막 문자 삭제
-        // B와 비교 같으면 count 출력
-        // 다르면 한번더 밀고 비교 반복
-        // 문자열의 길이-1번 반복 후 같지 않다면 -1 리턴
-        StringBuilder sb = new StringBuilder(A);
-        if(A.equals(B))
-            return 0;
-        for(int i=0;i<A.length();i++){
-            sb.insert(0,String.valueOf(sb.charAt(sb.length()-1)));
-            System.out.println("삽입결과:"+sb);
-            sb.deleteCharAt(sb.length()-1);
-            System.out.println("삭제결과:"+sb);
-            if(sb.toString().equals(B)){
-                return i+1;
-            }
-        }
-        return -1;
+
+        return (B+B).indexOf(A);
     }
 }
