1a08d71c793c0430b8538e89ef40e1c4e187d840
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-02-01 18:39:59 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.46 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,14 +1,5 @@
 class Solution {
     public String solution(int n) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=1;i<=n;i++){
-            if(i % 2 == 0){
-                sb.append("박");
-            }
-            else{
-                sb.append("수");
-            }
-        }
-        return sb.toString();
+        return new String(new char [n/2+1]).replace("\0", "수박").substring(0,n);
     }
 }
