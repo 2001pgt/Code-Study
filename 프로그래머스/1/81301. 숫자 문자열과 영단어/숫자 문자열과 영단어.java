00f0ace790633dd5893be8c1aafd1e61b3cdf2ff
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-02-24 23:22:13 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.13 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,17 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        StringBuilder sb = new StringBuilder("");
-        int len = s.length();
-        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
-        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
 
-        for(int i=0; i<10; i++){
-            s = s.replaceAll(alphabets[i],digits[i]);
+        String[][] mapArr = { {"0", "zero"}, 
+                              {"1", "one"}, 
+                              {"2", "two"}, 
+                              {"3", "three"}, 
+                              {"4", "four"}, 
+                              {"5", "five"}, 
+                              {"6", "six"}, 
+                              {"7", "seven"}, 
+                              {"8", "eight"}, 
+                              {"9", "nine"} };
+
+        for(String[] map : mapArr){
+            s = s.replace(map[1], map[0]);
         }
 
-        return Integer.parseInt(s);
+        int answer = Integer.parseInt(s);
+        return answer;
     }
 }
