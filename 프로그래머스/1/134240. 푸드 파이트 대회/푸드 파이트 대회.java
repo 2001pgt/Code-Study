0ebf6242a1734e5fd30e1bd6fb38cf2875a61f6f
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-02-25 23:14:05 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 4.52 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,13 +1,19 @@
+import java.util.Stack;
 class Solution {
     public String solution(int[] food) {
-        String answer = "0";
-
-        for (int i = food.length - 1; i > 0; i--) {
-            for (int j = 0; j < food[i] / 2; j++) {
-                answer = i + answer + i; 
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 1; i < food.length; i++) {
+            int tmp = food[i]/2;
+            for (int j = 0; j < tmp; j++) {
+                sb.append(i);
+                stack.push(i);
             }
         }
-
-        return answer;
+        sb.append(0);
+        while (!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.toString();
     }
 }
