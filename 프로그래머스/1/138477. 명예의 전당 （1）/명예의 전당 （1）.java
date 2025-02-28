24ef25ce639d8ea8aaf5486ebbb3cd266111d3c1
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-02-28 23:23:28 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.79 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,18 +2,13 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        ArrayList<Integer> numbers = new ArrayList<>();
-        // score를 처음부터 순회하며 한개씩 임시 리스트에 넣는다.
-        // 한번 리스트에 점수가 들어올때마다 정렬을 한다.
-        // 그리고 리스트의 크기가 k보다 커지면 가장 작은 점수를 삭제한다.
-        // 그리고 가장 낮은 점수를 answer에 추가한다.
+        PriorityQueue<Integer> numbers = new PriorityQueue<>();
         for(int i=0;i<score.length;i++){
             numbers.add(score[i]);
-            Collections.sort(numbers);
             if(numbers.size() > k){
-                numbers.remove(0);
+                numbers.remove();
             }
-            answer[i] = numbers.get(0);
+            answer[i] = numbers.peek();
         }
         return answer;
     }
