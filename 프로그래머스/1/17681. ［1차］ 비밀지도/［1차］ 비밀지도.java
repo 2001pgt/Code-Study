1bb7988ec857211e604144d3c013f7f6f86b744a
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-03-01 23:10:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.34 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,25 +1,15 @@
 class Solution {
-    public String makeSharp(int n, int m) {
-        if(n == 0) {
-            if( m > 0) {
-                String str = "";
-                for(int i = 0; i < m; i++) {
-                    str += " ";
-                }
-                return str;
-            }
-            else return "";
-        }
-        else {
-            return n % 2 == 0 ? makeSharp(n/2, m-1) + " " : makeSharp(n/2, m-1) + "#"; 
-        }
-    }
-    public String[] solution(int n, int [] arr1, int [] arr2) {
-        String [] answer = new String[n];
-        int [] secretMap = new int[n];
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
         for(int i = 0; i < n; i++) {
-            secretMap[i] = arr1[i] | arr2[i];
-            answer[i] = makeSharp(secretMap[i], n);
+            StringBuilder str = new StringBuilder();
+            int temp = arr1[i] | arr2[i];
+            int w = n;
+            while(w-- != 0) {
+                str.insert(0, (temp % 2) == 1 ? "#" : " ");
+                temp >>= 1;
+            }
+            answer[i] = String.valueOf(str);
         }
         return answer;
     }
