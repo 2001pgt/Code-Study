2f8bbd3ae93d41f020c3e1a52610c3e562fdfa91
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-03-01 23:14:18 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,19 +1,17 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        
         for (int i = 0; i < n; i++) {
-            // 두 숫자를 비트 OR 연산으로 결합
-            int num = arr1[i] | arr2[i];
-            
-            // n자리의 이진수 문자열 생성 (부족한 자리는 0으로 채움)
-            String binaryStr = String.format("%" + n + "s", Integer.toBinaryString(num))
-                                    .replace(' ', '0');
-            
-            // '1'은 '#'로, '0'은 공백으로 치환하여 최종 지도 문자열 생성
-            answer[i] = binaryStr.replace('0', ' ').replace('1', '#');
+            int combined = arr1[i] | arr2[i];
+            char[] row = new char[n];
+            // 오른쪽부터 한 자리씩 채워나가면서
+            // bit 값이 1이면 '#' 아니면 ' ' 할당
+            for (int j = n - 1; j >= 0; j--) {
+                row[j] = (combined & 1) == 1 ? '#' : ' ';
+                combined >>= 1;
+            }
+            answer[i] = new String(row);
         }
-        
         return answer;
     }
 }
