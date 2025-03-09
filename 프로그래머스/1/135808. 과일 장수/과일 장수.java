3bf322d9ee4de07dc776b94afd50f26fa6e40992
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-03-09 22:31:01 +0900

    Update 과일 장수.java

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,3 +1,30 @@
+메모리: 159 MB, 시간: 114.78 ms
+import java.util.*;
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        // 사과 배열을 정렬한다.
+        // 점수가 높은 사과부터 m개씩 담아서 점수를 계산한다.
+            // 정렬된 리스트의 m번째 원소의 점수 * m을 한 값을 answer에 추가한다
+        // 남은 사과의 개수가 m보다 적다면 종료
+        Arrays.sort(score);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : score) {
+            list.add(num);
+        }
+        Collections.reverse(list);
+        for(int i=0;i<score.length/m;i++){
+            answer += (list.get(i*m+m-1)*m);
+        }
+        // System.out.println(Arrays.toString(score));
+        // System.out.println(list);
+        return answer;
+    }
+}
+// 다른 풀이
+// 오름차순 정렬만  한뒤 for문을 배열의 끝에서 부터 시작해 시간을 줄였다.
+메모리: 136 MB, 시간: 51.69 ms
 import java.util.*;
 
 class Solution {
