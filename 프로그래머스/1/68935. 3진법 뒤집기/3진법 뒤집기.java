96bc0097a3a0c7fd4c1ebea09c58d48f754d4e88
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-03-27 21:00:17 +0900

    Update 3진법 뒤집기.java

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -10,3 +10,19 @@ class Solution {
         return decimalFromBase3;
     }
 }
+
+// 다른 풀이
+class Solution {
+    public int solution(int n) {
+        String a = "";
+
+        while(n > 0){
+            a = (n % 3) + a;
+            n /= 3;
+        }
+        a = new StringBuilder(a).reverse().toString();
+
+
+        return Integer.parseInt(a,3);
+    }
+}
