daffc940046e01ebc3cb9edb93ad30a85ddd89c2
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2025-08-16 20:39:54 +0900

    [Silver III] Title: 이친수, Time: 68 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 입력
+        int N = Integer.parseInt(br.readLine());
+        // D 배열 선언
+        long[][] D = new long[100][2];
+        // 초기값 설정
+        D[1][0] = 1;
+        D[2][0] = 1;
+        D[3][0] = 1;
+        D[3][1] = 1;
+        // 구현
+        for(int n=4;n<=N;n++){
+            D[n][1] = D[n-1][0];
+            D[n][0] = D[n-1][0]+D[n-1][1];
+        }
+        System.out.println(D[N][1]+D[N][0]);
+
+    }
+}
